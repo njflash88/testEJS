ef8e30db47f1db7ea76b6ebf8ae639d6b7886da8
njflash88 <gordonchu88@yahoo.com>
2025-03-21 03:19:45 +0800

    add app.set ejs to index.js #3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
   const data = {
